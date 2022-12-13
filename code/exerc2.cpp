498302d1f380b13b5ba7785541f85f253ad012a5
Magnus <91549385+KoksILasten@users.noreply.github.com>
2022-12-13 14:57:54 +0100

    Exect2 klar OBS! att orden den säger är annorlunda mot hur många det finns i hennes exempeloutput.

--- code/exerc2.cpp
+++ code/exerc2.cpp
@@ -15,13 +15,16 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <set>
+
+using table_entry = std::pair < std::string, std::set<std::string>>;
 
 int main() {
     // Create a map to store the subjects of words and their corresponding anagrams
-    std::map<std::string, std::vector<std::string>> subjects;
+    std::map<std::string, std::set<std::string>> subjects;
 
     // Open the input file
-    std::ifstream input("../uppgift2.txt");
+    std::ifstream input("../code/uppgift2.txt");
 
     if (!input) {
         std::cout << "Could not open input file!!\n";
@@ -32,45 +35,44 @@ int main() {
     //std::string word;
     std::vector<std::string> words;
     //std::istream_iterator<string>(input)
-    std::copy(std::istream_iterator<std::string>(input), std::istream_iterator<std::string>(), std::back_inserter(words)); // //std::copy takes an input iterator and an output iterator and copies the values from the input iterator to the output iterator.
+    std::copy(std::istream_iterator<std::string>(input), std::istream_iterator<std::string>(), std::back_inserter(words)); 
+    // std::copy takes an input iterator and an output iterator and copies the values from the input iterator to the output iterator.
     //while (input >> word) {
         // Sort the characters of the word alphabetically
-    std::sort(words.begin(), words.end());
+    //std::sort(words.begin(), words.end()); //not really needed
 
-    // Add the word to the list of anagrams for its subject
-    std::for_each(words.begin(), words.end(), [subjects](std::string aWord) {
-        //subjects[a].push_back(a); 
-        std::string copyWord{ aWord };
-    std::sort(aWord.begin(), aWord.end());
+    // Add the word to the list of anagrams for its subject'
 
-    auto found = std::find(subjects[copyWord].first(), subjects[copyWord].end(), aWord)
+    std::for_each(words.begin(), words.end(), [&subjects](std::string aWord) {
 
-        });
-    //subjects[words].push_back(words);
-//}
+        //subjects[a].push_back(a); //does not exist
+        //std::string copyWord{ aWord }; //
+        std::string temp = aWord;
+        	
+        std::sort(aWord.begin(), aWord.end());
+	    subjects[aWord].insert(temp);
+		});
+
 
 // Create an output file stream
-    std::ofstream output("output.txt");
+    std::ofstream output("../code/output.txt");
 
     // Write the number of words read from the input file
     output << "Total number of words: " << subjects.size() << std::endl;
 
     // Iterate over the map and write the anagrams for each subject
-    for (const auto& [subject, anagrams] : subjects) {
+    //for (const auto& [subject, anagrams] : subjects) {
+    std::for_each(subjects.begin(), subjects.end(), [&output](table_entry p) {
         // Write the anagrams only if there are two or more
-        if (anagrams.size() >= 2) {
-            output << "Subject: " << subject << std::endl;
+        if (p.second.size() >= 2) {
             output << "Anagrams: ";
 
             // Use the std::for_each algorithm to write the anagrams to the output stream
-            std::for_each(anagrams.begin(), anagrams.end(), [&output](const std::string& word) {
-                output << word << " ";
-                });
+            std::copy(p.second.begin(), p.second.end(), std::ostream_iterator<std::string>(output, " "));
 
-            // Write the number of anagrams for this subject
-            output << "(" << anagrams.size() << " words)" << std::endl;
+            output << "-->" << p.second.size() << " words" << std::endl;
         }
-    }
+        });
 
     return 0;
 }
